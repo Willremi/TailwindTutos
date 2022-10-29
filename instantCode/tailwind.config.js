9f3cda4927d108f55df60a12d301d54fc7de2ff6
Rémi Willebrouck <willremi@gmail.com>
2022-10-29 09:18:32 +0200

    fin du tutos

--- instantCode/tailwind.config.js
+++ instantCode/tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
